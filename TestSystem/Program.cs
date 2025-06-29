ff81057b353f51b505b876d2b3210bfa8be43641
AleksandrukV <valentynaleksandruk2005@gmail.com>
2025-06-30 00:19:15 +0300

    Fixed bug, garbage cleaning

--- TestSystem/Program.cs
+++ TestSystem/Program.cs
@@ -1,83 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-
-using Microsoft.AspNetCore.Cors.Infrastructure;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-
 using TestSystem.Data;
 using TestSystem.Models;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-
-//// Add services to the container.
-//builder.Services.AddRazorPages();
-
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//}
-
-//using (var scope = app.Services.CreateScope())
-//{
-//    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//    db.Database.Migrate();
-
-//    if (!db.Tests.Any())
-//    {
-//        db.Tests.Add(new Test
-//        {
-//            Title = "Базовий тест",
-//            Questions = new List<Question>
-//            {
-//                new Question
-//                {
-//                    Text = "Яка столиця України?",
-//                    Type = QuestionType.SingleChoice,
-//                    Answers = new List<Answer>
-//                    {
-//                        new Answer { Text = "Київ", IsCorrect = true },
-//                        new Answer { Text = "Львів", IsCorrect = false },
-//                        new Answer { Text = "Луцьк", IsCorrect = false }
-//                    }
-//                }
-//            }
-//        });
-//        db.SaveChanges();
-//    }
-//}
-
-
-
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//builder.Services.AddCors(options =>
-//{
-//    options.AddDefaultPolicy(policy =>
-//    {
-//        policy.AllowAnyOrigin()
-//              .AllowAnyHeader()
-//              .AllowAnyMethod();
-//    });
-//});
-
-//app.UseCors();
-
-//app.UseAuthorization();
-
-//app.MapRazorPages();
-
-//app.Run();
-
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -86,10 +9,6 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-// Configure the HTTP request pipeline.
-
 
 
 builder.Services.AddCors(options =>
@@ -116,6 +35,16 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.Migrate();
 
+    if (app.Environment.IsDevelopment())
+    {
+        db.Answers.RemoveRange(db.Answers);
+        db.Questions.RemoveRange(db.Questions);
+        db.Tests.RemoveRange(db.Tests);
+        db.UserAttempts.RemoveRange(db.UserAttempts);
+
+        db.SaveChanges();
+    }
+
     if (!db.Tests.Any())
     {
         db.Tests.Add(new Test
@@ -132,6 +61,38 @@ using (var scope = app.Services.CreateScope())
                         new Answer { Text = "Київ", IsCorrect = true },
                         new Answer { Text = "Львів", IsCorrect = false },
                         new Answer { Text = "Луцьк", IsCorrect = false }
+                    }
+                },
+                new Question
+                {
+                    Text = "Що із перечислених назв являється фруктом?",
+                    Type = QuestionType.MultipleChoice,
+                    Answers = new List<Answer>
+                    {
+                        new Answer { Text = "Яблуко", IsCorrect = true },
+                        new Answer { Text = "Груша", IsCorrect = true },
+                        new Answer { Text = "Помідор", IsCorrect = false }
+                    }
+                }
+            }
+        });
+
+        db.Tests.Add(new Test
+        {
+            Title = "Базовий тест 2",
+            Questions = new List<Question>
+            {
+                new Question
+                {
+                    Text = "Виберіть слова у ских є буква \'ч\'.",
+                    Type = QuestionType.MultipleChoice,
+                    Answers = new List<Answer>
+                    {
+                        new Answer { Text = "Леопард", IsCorrect = false },
+                        new Answer { Text = "Наряд", IsCorrect = false },
+                        new Answer { Text = "Ковпачок", IsCorrect = true },
+                        new Answer { Text = "Журавлі", IsCorrect = false },
+                        new Answer { Text = "Кольчуга", IsCorrect = true }
                     }
                 }
             }
