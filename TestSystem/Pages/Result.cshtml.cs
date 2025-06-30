62a7ee829828ce8a4f21391275f0cf36b8763b08
AleksandrukV <valentynaleksandruk2005@gmail.com>
2025-07-01 02:00:42 +0300

    rebuild take, added result

---
+++ TestSystem/Pages/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestSystem.Pages
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- TestSystem/Pages/Take.cshtml.cs
+++ TestSystem/Pages/Take.cshtml.cs
@@ -6,6 +6,38 @@ using TestSystem.Models;
 
 namespace TestSystem.Pages
 {
+    //public class TakeModel : PageModel
+    //{
+    //    private readonly ApplicationDbContext _context;
+
+    //    public TakeModel(ApplicationDbContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public Test Test { get; set; }
+
+    //    public IActionResult OnGet(int id)
+    //    {
+    //        Test = _context.Tests
+    //            .Include(t => t.Questions)
+    //                .ThenInclude(q => q.Answers)
+    //            .FirstOrDefault(t => t.Id == id);
+
+    //        if (Test == null)
+    //            return NotFound();
+
+    //        return Page();
+    //    }
+
+    //    public IActionResult OnPost()
+    //    {
+    //        // Обробка відповідей (опціонально)
+    //        return RedirectToPage("/Index");
+    //    }
+    //}
+
+
     public class TakeModel : PageModel
     {
         private readonly ApplicationDbContext _context;
@@ -15,14 +47,17 @@ namespace TestSystem.Pages
             _context = context;
         }
 
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+
         public Test Test { get; set; }
 
-        public IActionResult OnGet(int id)
+        public async Task<IActionResult> OnGetAsync()
         {
-            Test = _context.Tests
+            Test = await _context.Tests
                 .Include(t => t.Questions)
                     .ThenInclude(q => q.Answers)
-                .FirstOrDefault(t => t.Id == id);
+                .FirstOrDefaultAsync(t => t.Id == Id);
 
             if (Test == null)
                 return NotFound();
@@ -30,10 +65,45 @@ namespace TestSystem.Pages
             return Page();
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            // Обробка відповідей (опціонально)
-            return RedirectToPage("/Index");
+            var test = await _context.Tests
+                .Include(t => t.Questions)
+                    .ThenInclude(q => q.Answers)
+                .FirstOrDefaultAsync(t => t.Id == Id);
+
+            if (test == null)
+                return NotFound();
+
+            int correct = 0;
+
+            foreach (var question in test.Questions)
+            {
+                var formAnswers = Request.Form["Answers[" + question.Id + "]"];
+                if (formAnswers.ToString() != "")
+                {
+                    var selected = formAnswers.ToString().Split(',').Select(int.Parse).ToList();
+
+                    var correctAnswers = question.Answers
+                        .Where(a => a.IsCorrect)
+                        .Select(a => a.Id)
+                        .ToList();
+
+                    if (question.Type == QuestionType.SingleChoice)
+                    {
+                        if (selected.Count == 1 && correctAnswers.Contains(selected.First()))
+                            correct++;
+                    }
+                    else if (question.Type == QuestionType.MultipleChoice)
+                    {
+                        if (selected.OrderBy(x => x).SequenceEqual(correctAnswers.OrderBy(x => x)))
+                            correct++;
+                    }
+                }
+            }
+
+            TempData["Result"] = $"Правильних відповідей: {correct} з {test.Questions.Count}";
+            return RedirectToPage("/Result");
         }
     }
 
